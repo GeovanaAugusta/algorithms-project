58f4b92ccbdae4106446d5a36a63dba42814fd44
Geovana Augusta <geovanaag@hotmail.com>
2023-01-25 20:05:18 -0300

    feat: def that verify if a word is a palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,24 @@
+# Uma função que chama a si mesma é chamada de recursiva.
+# O processo para executar tal função recursiva é chamado de recursividade.
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    print(word)
+    print("low_index", low_index, "high_index", high_index)
+
+    if not word or word == '':
+        return False
+
+    if word != word[::-1]:
+        return False
+
+    print(low_index > high_index)
+    if low_index > high_index:
+        return True
+
+    # print(is_palindrome_recursive(word, low_index + 1, high_index - 1))
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+# SOURCE
+# 3
+# https://www.mygreatlearning.com/blog/palindrome-in-python/
+#  reversed_string == string[::-1]
