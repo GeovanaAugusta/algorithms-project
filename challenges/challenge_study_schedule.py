b10027695ccf41a5f4e671cb8cd487f0df6cee0d
Geovana Augusta <geovanaag@hotmail.com>
2023-01-25 16:51:31 -0300

    feat: def that verify how many students studying in the same hour

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,22 @@
+# permanence_period = entrada e saída (1, 2)
+# target_time = intervalo que quero a consulta
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
-# iniitial commit
+
+    count = 0
+
+    if len(permanence_period) == 0 or not target_time:
+        return None
+
+    if not all(all(isinstance(i, int) for i in tup)
+               for tup in permanence_period):
+        return None
+
+    # Desestruturação no Python
+    for begin, last in permanence_period:
+        print("tup", begin, last)
+        print("cond", begin <= target_time <= last)
+        if begin <= target_time <= last:
+            count += 1
+    print("count", count)
+    return count
